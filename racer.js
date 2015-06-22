1a89b23f0dfd5112913c38e45a5d88b5be0e819e
Andrew Donato <andrew.donato91@gmail.com>
2015-06-21 19:20:13 -0700

    racers now move on button press, no logic yet for declaring a winner or ending the game

--- racer.js
+++ racer.js
@@ -1,109 +1,193 @@
-var Player = function(name) {
+//  model
+var Player = function(name){
   var name = name;
   var currentPosition = 0;
 
-  this.getCurrentPosition = function() {
+  this.move = function(){
+    currentPosition++;
+  }
+
+  this.getPosition = function(){
     return currentPosition;
   }
 
-  this.getName = function() {
+  this.getName = function(){
     return name;
   }
-
-  this.move = function() {
-    currentPosition++;
-  }
 }
 
-var GameView = function() {
-
-  function clearActivePosition(track) {
-    track.find(".active").removeClass("active");
-  }
-
-  function getPlayerTrackByName(name) {
-    return $("#" + name + "_strip");
-  }
-
-  function resetTracks(players) {
-    $.each(players, function(keyCode, player) {
-      var track = getPlayerTrackByName(player.getName());
-      clearActivePosition(track);
-      $(track.children()[0]).addClass("active");
-    });
-  }
-
-  function clearWinner() {
-    $("#winner").empty();
-  }
-
-  this.getPage = function() {
+//  view
+var GameBoard = function(){
+  this.getBoard = function() {
     return $(document);
   }
-
-  this.getFinishPosition = function(name) {
-    return getPlayerTrackByName(name).children().length - 1;
-  }
-
-  this.updatePlayerPosition = function(name, position) {
-    track = getPlayerTrackByName(name);
-    clearActivePosition(track);
-    $(track.children()[position]).addClass("active");
-  }
-
-  this.reportWinner = function(name) {
-    $("#winner").append("<h1>" + name + " Won!</h1><input type='button' value='Reset Game' name='reset'>");
-  }
-
-  this.resetGame = function(players) {
-    resetTracks(players);
-    clearWinner();
+  this.updatePlayerPosition = function(player){
+    var track = $("#" + player.getName() + "_strip");
+    // .find searches its children for the .active cell
+    track.find(".active").removeClass("active");
+    // .children gives an array of children
+    $(track.children()[player.getPosition()]).addClass("active");
   }
 }
 
+//  controller
 var Game = function() {
-  var gameView = new GameView();
 
+  var gameBoard = new GameBoard();
   var players = {
     81: new Player("player1"),
     80: new Player("player2")
   }
 
-  function advanceRacer(event) {
-    event.preventDefault();
-    processPlayerTurn(players[event.keyCode]);
-  }
-
-  function processPlayerTurn(player) {
-    movePlayer(player);
-    determineWinner(player);
-  }
-
-  function movePlayer(player) {
+  function advanceRacer(event){
+    var player = players[event.keyCode];
     player.move();
-    gameView.updatePlayerPosition(player.getName(), player.getCurrentPosition());
+    gameBoard.updatePlayerPosition(player);
   }
 
-  function determineWinner(player) {
-    var finishPosition = gameView.getFinishPosition(player.getName());
-
-    if(player.getCurrentPosition() == finishPosition) {
-      finishGame(player.getName());
-    }
-  }
-
-  function finishGame(winner) {
-    gameView.getPage().off("keyup");
-    gameView.reportWinner(winner);
-  }
-
-  function resetGame() {
-    gameView.resetGame(players);
-    gameView.getPage().on("keyup", advanceRacer);
-  }
-
-  this.initialize =  function() {
-    gameView.getPage().on("keyup", advanceRacer);
-    gameView.getPage().on("click", "input[name='reset']", resetGame);
+  this.initialize = function() {
+    gameBoard.getBoard().on("keyup", advanceRacer);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// var Player = function(name) {
+//   var name = name;
+//   var currentPosition = 0;
+
+//   this.getCurrentPosition = function() {
+//     return currentPosition;
+//   }
+
+//   this.getName = function() {
+//     return name;
+//   }
+
+//   this.move = function() {
+//     currentPosition++;
+//   }
+// }
+
+// var GameView = function() {
+
+//   function clearActivePosition(track) {
+//     track.find(".active").removeClass("active");
+//   }
+
+//   function getPlayerTrackByName(name) {
+//     return $("#" + name + "_strip");
+//   }
+
+//   function resetTracks(players) {
+//     $.each(players, function(keyCode, player) {
+//       var track = getPlayerTrackByName(player.getName());
+//       clearActivePosition(track);
+//       $(track.children()[0]).addClass("active");
+//     });
+//   }
+
+//   function clearWinner() {
+//     $("#winner").empty();
+//   }
+
+//   this.getPage = function() {
+//     return $(document);
+//   }
+
+//   this.getFinishPosition = function(name) {
+//     return getPlayerTrackByName(name).children().length - 1;
+//   }
+
+//   this.updatePlayerPosition = function(name, position) {
+//     track = getPlayerTrackByName(name);
+//     clearActivePosition(track);
+//     $(track.children()[position]).addClass("active");
+//   }
+
+//   this.reportWinner = function(name) {
+//     $("#winner").append("<h1>" + name + " Won!</h1><input type='button' value='Reset Game' name='reset'>");
+//   }
+
+//   this.resetGame = function(players) {
+//     resetTracks(players);
+//     clearWinner();
+//   }
+// }
+
+// var Game = function() {
+//   var gameView = new GameView();
+
+//   var players = {
+//     81: new Player("player1"),
+//     80:qqq new Player("player2")
+//   }
+
+//   function advanceRacer(event) {
+//     event.preventDefault();
+//     processPlayerTurn(players[event.keyCode]);
+//   }
+
+//   function processPlayerTurn(player) {
+//     movePlayer(player);
+//     determineWinner(player);
+//   }
+
+//   function movePlayer(player) {
+//     player.move();
+//     gameView.updatePlayerPosition(player.getName(), player.getCurrentPosition());
+//   }
+
+//   function determineWinner(player) {
+//     var finishPosition = gameView.getFinishPosition(player.getName());
+
+//     if(player.getCurrentPosition() == finishPosition) {
+//       finishGame(player.getName());
+//     }
+//   }
+
+//   function finishGame(winner) {
+//     gameView.getPage().off("keyup");
+//     gameView.reportWinner(winner);
+//   }
+
+//   function resetGame() {
+//     gameView.resetGame(players);
+//     gameView.getPage().on("keyup", advanceRacer);
+//   }
+
+//   this.initialize =  function() {
+//     gameView.getPage().on("keyup", advanceRacer);
+//     gameView.getPage().on("click", "input[name='reset']", resetGame);
+//   }
+// }
